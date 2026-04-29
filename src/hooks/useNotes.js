d7b9c663da675b5ba1a83915435cf30426c9accc
Fernando <fernanguedezr@gmail.com>
2026-04-29 19:05:19 +0200

    feat: add new components and context

--- src/hooks/useNotes.js
+++ src/hooks/useNotes.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { isRunnableDevEnvironment } from "vite";
 
 const MAX_NOTES = 10;
 const STORAGE_KEY = "sticky-notes";
@@ -21,24 +20,22 @@ function saveNotes(notes) {
 export function useNotes() {
   const [notes, setNotes] = useState(loadNotes);
 
-  //añadir notas
   function addNote(text, color) {
-    //si después de quitar los espacios el texto queda vacío (""), js lo considera "falsy"
-    // al negarlo con !, lo convierte en true
     if (!text.trim()) return { error: "empty" };
     if (notes.length >= MAX_NOTES) return { error: "max" };
 
     const newNote = { id: uuidv4(), text, color };
-    const updated = [newNote, notes];
+    const updated = [newNote, ...notes];
     setNotes(updated);
     saveNotes(updated);
     return { error: null };
   }
-  // eliminar notas
+
   function deleteNote(id) {
-    const updated = notes.filter((notes) => notes.id !== id);
+    const updated = notes.filter((note) => note.id !== id);
     setNotes(updated);
     saveNotes(updated);
   }
+
   return { notes, addNote, deleteNote };
 }
