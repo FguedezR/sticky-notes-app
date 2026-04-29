f57c8c8e284af71d873d5874248899a3f615bd7a
Fernando <fernanguedezr@gmail.com>
2026-04-29 18:12:21 +0200

    feat: add new components and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { ColorProvider } from "./context/ColorContext";
+
 function App() {
   return (
     <div className="app-container">
       <h1>🗒️ Sticky Notes</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
